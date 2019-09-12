49d6952cabd590ea03427036ca9452f3f7a1a7a2
Rafael R. Camilo Mançan <rafael.mancan@gmail.com>
2019-09-12 14:03:54 -0300

    adonis tdd api

---
+++ database/factory.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+|--------------------------------------------------------------------------
+| Factory
+|--------------------------------------------------------------------------
+|
+| Factories are used to define blueprints for database tables or Lucid
+| models. Later you can use these blueprints to seed your database
+| with dummy data.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+
+Factory.blueprint('App/Models/User', faker => {
+  return {
+    username: faker.username(),
+    email: faker.email(),
+    password: 'password123'
+  };
+});
+
+Factory.blueprint('App/Models/Challenge', faker => {
+  return {
+    title: faker.sentence(),
+    description: faker.sentence()
+  };
+});
